e908f90e2f4b9bdd6f6b9e16f539873bcc9fcb48
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-05 12:45:36 -0500

    added most of drill #3, but getting an error of undefined item name

--- scripts/store.js
+++ scripts/store.js
@@ -11,10 +11,51 @@ const store = (function () {
   let hideCheckedItems = false;
   const searchTerm = '';
 
+  const addItem = function(name){
+    try {
+      Item.validateName(name);
+      store.items.push(this.items);
+    } catch(error) {
+      console.log('Cannot add item:' + error.message);
+    }
+  };
+
+  const findById = function(id){
+    return store.items.find(item => item.id === id);
+  };
+
+  const findAndToggleChecked = function(id) {
+    this.findById(id).checked = !this.findById(id).checked;
+  };
+
+  // >>> here!
+  const findAndDelete = function(id) {
+    const index = this.findById(id);
+    const item = store.items.findIndex(index);
+    store.items.splice(item, 1);
+  };
+
+
+  const findAndUpdateName = function(id, newName) {
+    try {
+      Item.validateName(name);
+      store.items.findById(id).name = newName; 
+      
+    } catch(error) {
+      console.log(`Cannot update name: ${error.message}`);
+    }
+  };
+
+
   return {
     items,
     hideCheckedItems,
-    searchTerm
+    searchTerm,
+    addItem,
+    findById,
+    findAndToggleChecked,
+    findAndDelete,
+    findAndUpdateName
   };
 
 }() );
