87474e2324e4df33de0158b0c92d218d47985669
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-05 08:54:27 -0800

    using Item.js functions to render new list items

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -58,7 +58,14 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try {
+      Item.validateName(itemName);
+      store.items.push(Item.create(itemName));
+      render();
+    } catch(error) {
+      console.error(`Cannot add item: ${error.message}`);
+    }
+    //store.items.push({ id: cuid(), name: itemName, checked: false });
   }
   
   function handleNewItemSubmit() {
