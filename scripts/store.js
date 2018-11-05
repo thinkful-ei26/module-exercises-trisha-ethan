311b398cd79f3b4f89b693376ac52323de0e671f
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-05 11:07:05 -0500

    added store.js

---
+++ scripts/store.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const store = (function () {
+  const items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+
+  let hideCheckedItems = false;
+  const searchTerm = '';
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm
+  };
+
+}() );
